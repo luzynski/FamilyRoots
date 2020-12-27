17ada57d916e4138c73f90530e9c581a80d6d571
luzynski <luzynski@gmail.com>
2020-12-27 21:23:22 +0000

    Starting from scratch. Generated projects.

---
+++ FamilyRoots.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FamilyRoots.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- FamilyRoots.Data/Note.cs
+++
@@ -1,7 +0,0 @@
-namespace FamilyRoots.Data
-{
-    public class Note
-    {
-        
-    }
-}
